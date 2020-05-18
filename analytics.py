790a34b17fcef02db14c857546176a5bf6e6a019
Gannon Barnett <barnettg@mit.edu>
2020-05-18 19:03:27 -0400

    Added avg and total num capabilities

--- analytics.py
+++ analytics.py
@@ -24,8 +24,12 @@ class DayAnalytics:
                     new_freq_map[lower_cased] = 1
         self.word_freq_map = new_freq_map
     
-    def getWordFreq(self, word):
-        return self.word_freq_map[word] if word in self.word_freq_map else 0 
+    def getWordFreqs(self, words, use_avg=False):
+        total = 0 
+        for word in words: 
+            if word in self.word_freq_map:
+                total += self.word_freq_map[word]
+        return float(total) / float(len(self.confessions)) if use_avg else float(total)
 
     def getPostedConfessiosn(self):
         return len(self.confessions)
@@ -46,16 +50,31 @@ class Analytics:
                 if current_day == None:
                     current_day = DayAnalytics(row_date)
                 elif current_day.date != row_date:
+                    #finalize analytics of last day and add to days
                     current_day.updateAnalytics()
                     days.append(current_day)
+
+                    #then switch to new day 
                     current_day = DayAnalytics(row_date)
                 
                 current_day.confessions.append(row[1])
             days.append(current_day)
         return days
 
-    def getWordFreqData(self, word):
-        return [[day.getWordFreq(word) for day in self.days_aggregate], "Word Usages"]
+    def createBaseDailyFigure(self, custom_range=None):
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.set_xlabel('Date posted')
+        xticks = []
+        for i in range(len(self.days_aggregate)): 
+            if self.days_aggregate[i].date.day == 1:
+                #put xtick on first day of month 
+                #todo; this assume ocnfessions posted every day 
+                xticks.append({'index':i, 'label': self.days_aggregate[i].date.month})
+        axis.set_xticks([x['index'] for x in xticks])
+        axis.set_xticklabels([MONTHS_OF_YEAR[x['label']] for x in xticks])
+
+        return fig
 
     def createDailyFigure(self, ys, y_label, NUM_Y_TICKS=9.0):
         fig = Figure()
@@ -77,3 +96,18 @@ class Analytics:
         y_tick_interval = (max(ys)+1.0) / NUM_Y_TICKS + 0.1; 
         axis.set_yticks(np.arange(0, max(ys)+1, math.ceil(y_tick_interval)))
         return fig
+
+    def addWordFrequency(self, fig, words, color='blue', use_avg=False): 
+        axis = fig.add_subplot(1, 1, 1)
+        xs = range(len(self.days_aggregate))
+        word_freq_data = [day.getWordFreqs(words, use_avg=use_avg) for day in self.days_aggregate]
+        axis.bar(xs, word_freq_data, color=color)
+        return fig 
+
+    def addNumLine(self, fig):
+        axis = fig.add_subplot(1, 1, 1)
+        axis2 = axis.twinx()
+        xs = range(len(self.days_aggregate))
+        day_count_data = [len(day.confessions) for day in self.days_aggregate]
+        axis2.plot(xs, day_count_data, color='black')
+        return fig 
--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template 
+from flask import Flask, Response, render_template, request
 from flask import Response
 
 from analytics import Analytics
@@ -6,19 +6,26 @@ import io
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 app = Flask(__name__)
-MAY_2020_DATA_PATH = "data_5_2020.csv"
+DATA_SOURCE = "data_5_2020.csv"
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/word_freq/<word>/plot.png')
-def generate_plot(word):
-    fig = AnalObj.createDailyFigure(*AnalObj.getWordFreqData(word))
+@app.route('/plot.png')
+def generate_plot():
+    # todo change design to support multiple types of queiries, not just word freq analysis.. 
+    word_freq_targets = request.args.get('wf')
+    show_num = request.args.get('show_num') == 'true'
+    use_avg = request.args.get('use_avg') == 'true'
+    fig = AnalObj.createBaseDailyFigure()
+    fig = AnalObj.addWordFrequency(fig, word_freq_targets.split(","), color='blue', use_avg=use_avg)
+    if (show_num):
+        fig = AnalObj.addNumLine(fig)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == "__main__":     
-    AnalObj = Analytics(MAY_2020_DATA_PATH)
+    AnalObj = Analytics(DATA_SOURCE)
     app.run(debug=True)
